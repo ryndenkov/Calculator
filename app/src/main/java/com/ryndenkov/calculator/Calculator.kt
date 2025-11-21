9bffc39ed307b2e973126e0e87c91ea3e33a15fc
arynd <a.ryndenkov2001@yandex.ru>
2025-11-21 22:59:41 +0300

    Update design

--- app/src/main/java/com/ryndenkov/calculator/Calculator.kt
+++ app/src/main/java/com/ryndenkov/calculator/Calculator.kt
@@ -8,33 +8,49 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.ryndenkov.calculator.ui.theme.CalculatorTheme
 
-@Preview
-@Composable
-fun CalculatorPreview() {
-    CalculatorTheme { }
-}
+//@Preview
+//@Composable
+//fun CalculatorPreview() {
+//    CalculatorTheme { }
+//}
 
 @Composable
 fun Calculator(modifier: Modifier = Modifier) {
     Column(
         modifier = modifier
-            .fillMaxSize()
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         Column(
             modifier = Modifier
+                .clip(
+                    RoundedCornerShape(
+                        topStart = 0.dp,
+                        topEnd = 0.dp,
+                        bottomStart = 40.dp,
+                        bottomEnd = 40.dp
+                    )
+                )
                 .fillMaxWidth()
-                .background(MaterialTheme.colorScheme.primaryContainer),
+                .background(MaterialTheme.colorScheme.primaryContainer)
+                .weight(1f)
+                .padding(bottom = 16.dp, end = 40.dp, start = 40.dp),
             verticalArrangement = Arrangement.Bottom,
             horizontalAlignment = Alignment.End
         ) {
@@ -85,13 +101,17 @@ fun Calculator(modifier: Modifier = Modifier) {
                 color = MaterialTheme.colorScheme.onBackground
             )
         }
-        Row(modifier = Modifier.fillMaxWidth()) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
             Box(
                 modifier = Modifier
+                    .clip(CircleShape)
                     .weight(1f)
                     .background(MaterialTheme.colorScheme.secondary)
                     .aspectRatio(1f),
-                contentAlignment = Alignment.Center
+                contentAlignment = Alignment.Center,
             ) {
                 Text(
                     text = "AC",
@@ -101,6 +121,7 @@ fun Calculator(modifier: Modifier = Modifier) {
             }
             Box(
                 modifier = Modifier
+                    .clip(CircleShape)
                     .weight(1f)
                     .background(MaterialTheme.colorScheme.tertiary)
                     .aspectRatio(1f),
@@ -114,6 +135,7 @@ fun Calculator(modifier: Modifier = Modifier) {
             }
             Box(
                 modifier = Modifier
+                    .clip(CircleShape)
                     .weight(1f)
                     .background(MaterialTheme.colorScheme.tertiary)
                     .aspectRatio(1f),
@@ -127,6 +149,7 @@ fun Calculator(modifier: Modifier = Modifier) {
             }
             Box(
                 modifier = Modifier
+                    .clip(CircleShape)
                     .weight(1f)
                     .background(MaterialTheme.colorScheme.tertiary)
                     .aspectRatio(1f),
@@ -139,9 +162,13 @@ fun Calculator(modifier: Modifier = Modifier) {
                 )
             }
         }
-        Row(modifier = Modifier.fillMaxWidth()) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
             Box(
                 modifier = Modifier
+                    .clip(CircleShape)
                     .weight(1f)
                     .background(MaterialTheme.colorScheme.primary)
                     .aspectRatio(1f),
@@ -155,6 +182,7 @@ fun Calculator(modifier: Modifier = Modifier) {
             }
             Box(
                 modifier = Modifier
+                    .clip(CircleShape)
                     .weight(1f)
                     .background(MaterialTheme.colorScheme.primary)
                     .aspectRatio(1f),
@@ -168,6 +196,7 @@ fun Calculator(modifier: Modifier = Modifier) {
             }
             Box(
                 modifier = Modifier
+                    .clip(CircleShape)
                     .weight(1f)
                     .background(MaterialTheme.colorScheme.primary)
                     .aspectRatio(1f),
@@ -181,6 +210,7 @@ fun Calculator(modifier: Modifier = Modifier) {
             }
             Box(
                 modifier = Modifier
+                    .clip(CircleShape)
                     .weight(1f)
                     .background(MaterialTheme.colorScheme.tertiary)
                     .aspectRatio(1f),
@@ -193,9 +223,13 @@ fun Calculator(modifier: Modifier = Modifier) {
                 )
             }
         }
-        Row(modifier = Modifier.fillMaxWidth()) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
             Box(
                 modifier = Modifier
+                    .clip(CircleShape)
                     .weight(1f)
                     .background(MaterialTheme.colorScheme.primary)
                     .aspectRatio(1f),
@@ -209,6 +243,7 @@ fun Calculator(modifier: Modifier = Modifier) {
             }
             Box(
                 modifier = Modifier
+                    .clip(CircleShape)
                     .weight(1f)
                     .background(MaterialTheme.colorScheme.primary)
                     .aspectRatio(1f),
@@ -222,6 +257,7 @@ fun Calculator(modifier: Modifier = Modifier) {
             }
             Box(
                 modifier = Modifier
+                    .clip(CircleShape)
                     .weight(1f)
                     .background(MaterialTheme.colorScheme.primary)
                     .aspectRatio(1f),
@@ -235,6 +271,7 @@ fun Calculator(modifier: Modifier = Modifier) {
             }
             Box(
                 modifier = Modifier
+                    .clip(CircleShape)
                     .weight(1f)
                     .background(MaterialTheme.colorScheme.tertiary)
                     .aspectRatio(1f),
@@ -247,9 +284,13 @@ fun Calculator(modifier: Modifier = Modifier) {
                 )
             }
         }
-        Row(modifier = Modifier.fillMaxWidth()) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
             Box(
                 modifier = Modifier
+                    .clip(CircleShape)
                     .weight(1f)
                     .background(MaterialTheme.colorScheme.primary)
                     .aspectRatio(1f),
@@ -263,6 +304,7 @@ fun Calculator(modifier: Modifier = Modifier) {
             }
             Box(
                 modifier = Modifier
+                    .clip(CircleShape)
                     .weight(1f)
                     .background(MaterialTheme.colorScheme.primary)
                     .aspectRatio(1f),
@@ -276,6 +318,7 @@ fun Calculator(modifier: Modifier = Modifier) {
             }
             Box(
                 modifier = Modifier
+                    .clip(CircleShape)
                     .weight(1f)
                     .background(MaterialTheme.colorScheme.primary)
                     .aspectRatio(1f),
@@ -289,6 +332,7 @@ fun Calculator(modifier: Modifier = Modifier) {
             }
             Box(
                 modifier = Modifier
+                    .clip(CircleShape)
                     .weight(1f)
                     .background(MaterialTheme.colorScheme.tertiary)
                     .aspectRatio(1f),
@@ -302,9 +346,13 @@ fun Calculator(modifier: Modifier = Modifier) {
             }
 
         }
-        Row(modifier = Modifier.fillMaxWidth()) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
             Box(
                 modifier = Modifier
+                    .clip(CircleShape)
                     .weight(2f)
                     .background(MaterialTheme.colorScheme.primary)
                     .aspectRatio(2f),
@@ -318,6 +366,7 @@ fun Calculator(modifier: Modifier = Modifier) {
             }
             Box(
                 modifier = Modifier
+                    .clip(CircleShape)
                     .weight(1f)
                     .background(MaterialTheme.colorScheme.primary)
                     .aspectRatio(1f),
@@ -331,6 +380,7 @@ fun Calculator(modifier: Modifier = Modifier) {
             }
             Box(
                 modifier = Modifier
+                    .clip(CircleShape)
                     .weight(1f)
                     .background(MaterialTheme.colorScheme.tertiary)
                     .aspectRatio(1f),
--- app/src/main/java/com/ryndenkov/calculator/MainActivity.kt
+++ app/src/main/java/com/ryndenkov/calculator/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : ComponentActivity() {
             CalculatorTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Calculator(
-                        modifier = Modifier.padding(innerPadding)
+                        modifier = Modifier.padding(bottom = innerPadding.calculateBottomPadding())
                     )
                 }
             }
