c66ee0ede2052c7cc9a914b584cf5847262a42ac
arynd <a.ryndenkov2001@yandex.ru>
2025-11-21 21:47:15 +0300

    Create theme

--- app/src/main/java/com/ryndenkov/calculator/ui/theme/Color.kt
+++ app/src/main/java/com/ryndenkov/calculator/ui/theme/Color.kt
@@ -2,10 +2,18 @@ package com.ryndenkov.calculator.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val White = Color(color = 0xFFFFFFFF)
+val Black = Color(color = 0xFF000000)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Grey100 = Color(color = 0xCCFFFFFF)
+val Grey200 = Color(color = 0xFFE6E7EB)
+val Grey300 = Color(color = 0xFFD9D9D9)
+val Grey400 = Color(color = 0x802C2E33)
+val Grey500 = Color(color = 0xFF333333)
+val Grey600 = Color(color = 0xE5000000)
+
+val Green100 = Color(color = 0x80006600)
+val Green200 = Color(color = 0xCC006600)
+
+val Blue100 = Color(color = 0x80084276)
+val Blue200 = Color(color = 0xCC084276)
--- app/src/main/java/com/ryndenkov/calculator/ui/theme/Theme.kt
+++ app/src/main/java/com/ryndenkov/calculator/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.ryndenkov.calculator.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -12,15 +11,25 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
+    background = Black,
+    onBackground = White,
+    primary = Grey400,
+    secondary = Green200,
+    tertiary = Blue200,
+    primaryContainer = Grey500,
+    onPrimaryContainer = White,
+    onSecondaryContainer = Grey100
+    )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    background = White,
+    onBackground = Grey600,
+    primary = Grey200,
+    secondary = Green100,
+    tertiary = Blue100,
+    primaryContainer = Grey300,
+    onPrimaryContainer = Black,
+    onSecondaryContainer = Black
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -37,7 +46,7 @@ private val LightColorScheme = lightColorScheme(
 fun CalculatorTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
